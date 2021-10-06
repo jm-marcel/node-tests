eda05ca7eb521028293156f466a7ee697a8ceee7
João Marcel <48781438+jm-marcel@users.noreply.github.com>
2021-10-06 11:38:01 -0300

    Update contactDAO.js

--- contact/lib/contactDAO.js
+++ contact/lib/contactDAO.js
@@ -1,31 +1,29 @@
 module.exports = (contact) => {
-  if (!contact.email) {
-    throw "Insira um email válido!";
-  }
 
-  if (!contact.password) {
-    throw "Insira uma senha válida!";
-  }
-
-  if (!contact.name) {
-    throw "Insira um nome válido!";
-  }
-
-  if (
-    !contact.age ||
-    contact.age === 0 ||
-    contact.age < 5 ||
-    contact.age > 99
-  ) {
-    throw "Insira uma idade válida!";
-  }
-
-  if (
-    !contact.weight ||
-    contact.weight === 0 ||
-    contact.weight < 1 ||
-    contact.weight > 200
-  ) {
-    throw "Insira um peso válido!";
-  }
+    switch (true) {
+        case !contact.email: {
+            throw "Insira um email válido!";
+        }
+        case !contact.password: {
+            throw "Insira uma senha válida!";
+        }
+        case !contact.name: {
+            throw "Insira um nome válido!";
+        }
+        case
+        !contact.age ||
+        contact.age === 0 ||
+        contact.age < 5 ||
+        contact.age > 99: {
+            throw "Insira uma idade válida!";
+        }
+        case
+        !contact.weight ||
+        contact.weight === 0 ||
+        contact.weight < 1 ||
+        contact.weight > 200: {
+            throw "Insira um peso válido!";
+        }
+    }
+    
 };
